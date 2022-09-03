3e313d7f82c5fdda17cbe28af1dc9bca164e0f6f
bezbiletniy <a.borodin.ca@gmail.com>
2022-09-03 16:53:44 +0300

    Test3 JSON

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTestV4 {
     @Test
@@ -20,5 +21,35 @@ class MobileBankApiTestV4 {
           .statusCode(200)
           .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
       ;
+    }
+
+    @Test
+    void shouldRubCurrency() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("[2].currency", equalTo("RUB"))
+        ;
+    }
+
+    @Test
+    void shouldUsdCurrency() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("[1].currency", equalTo("USD"))
+        ;
     }
 }
